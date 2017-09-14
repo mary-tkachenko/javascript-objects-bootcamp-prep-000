3ba4d3760327423e04fe9436004863b38b4c63b3
Mary <l.skyling@gmail.com>
2017-09-14 14:26:57 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,7 @@ var playlist = {
   artistName: "Cocorosie",
   songTitle: "Lemonade"
 };
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign(playlist, {[artistName: songTitle]})
+}
